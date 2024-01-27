df570b2ae4b237ece3f5dfb51bc9ad686943be39
Ilia Miheev <imiheev774@gmail.com>
2024-01-27 16:38:20 +0300

    finaly commit

--- chat.py
+++ chat.py
@@ -1,7 +1,11 @@
+# создание переменных, импорт библиотек
 from os import name, system
 import datetime
 oneTime = datetime.datetime.now()
+stop = False
+listMess = []
 
+# функция для очистки консоли
 def clear_console():
     operating_system = name
     if operating_system == 'posix':
@@ -11,6 +15,7 @@ def clear_console():
     else:
         print("Очистка консоли не поддерживается на данной операционной системе.")
 
+# две функции для редактирования текста
 def capitalize_after_dot(string):
     words = string.split('. ')
     new_string = []
@@ -25,13 +30,22 @@ def capitalize_after_space(word):
         word = word.replace(".", ". ")
         word = word.replace(' .', '.')
         word = word.replace(' ,', ',')
+        word = word.replace(' что ', ', что')
+        word = word.replace(' то ', ', то')
         word = " ".join(word.replace(" ", " ").split())
         word = capitalize_after_dot(word)
         if word and word[-1] not in ['.', '!', '?']:
             word = word[0].upper() + word[1:]
-        
     return word
 
+# вывод приветствия
+clear_console()
+print('Эта программа создана, чтобы вы могли побеседовать с самим собой')
+print()
+a = input('Продолжить? ')
+clear_console()
+
+# вывод всех сообщений
 def printAllMess():
     clear_console()
     for index, mess in enumerate(listMess):
@@ -40,19 +54,9 @@ def printAllMess():
         else:
             print(f'\033[93m {mess}\033[0m')
 
-stop = False
-listMess = []
-
-clear_console()
-print('Эта программа создана, чтобы вы могли побеседовать с самим собой')
-print()
-
-a = input('Продолжить? ')
-clear_console()
-
+# основной код
 while not stop:
-    message = input(' Введите сообщение: ')
-
+    message = input(' В: ')
     if message.lower() in ['стоп', 'exit']:
         stop = True
         printAllMess()
@@ -60,18 +64,18 @@ while not stop:
     elif not message.strip():
         while not message.strip():
             printAllMess()
-            message = input(' Введите сообщение: ')
+            message = input(' В: ')
     else:
         listMess.append(capitalize_after_space(message))
         printAllMess()
+
+# определяем время
 twoTime = datetime.datetime.now()
 allSecond = round((twoTime - oneTime).total_seconds())
 minutes = allSecond // 60
 remaining_seconds = allSecond % 60
 
-print(" Ты разговаривал:")
+# вывоим время разговора
+print(" Время разговора:")
 print("     Минут:", minutes)
 print("     Секунд:", remaining_seconds)
-
-
-
